ea603a1c2582ca1d3d9ce3fd82da6daf8bf1402c
CarlosMoises20 <carlosmoisestavares@gmail.com>
2025-04-09 23:53:29 +0000

    remove CrateDB constants

--- common/constants.py
+++ common/constants.py
@@ -12,9 +12,3 @@ SPARK_NETWORK_TIMEOUT = "180s"
 SPARK_EXECUTOR_HEARTBEAT_INTERVAL = "30s"
 SPARK_NUM_PARTITIONS = "10" 
 SPARK_FILES_MAX_PARTITION_BYTES = "134217728"
-
-
-'''CRATEDB'''
-CRATEDB_HOST = 'tfm-ct.dyn.fil.isel.pt'         # or another from your choice
-CRATEDB_PORT = 4201                             # or another from your choice (it must be the same port of the container)
-CRATEDB_URI = f'http://{CRATEDB_HOST}:{CRATEDB_PORT}'
